9895b7a1e6771b7379a1b4eb1c865de082b6b065
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-04-09 08:02:12 +0200

    log

--- test/smoke/src/areas/terminal/terminal-editors.test.ts
+++ test/smoke/src/areas/terminal/terminal-editors.test.ts
@@ -9,7 +9,7 @@ export function setup() {
 	describe('Terminal Editors', () => {
 		let terminal: Terminal;
 		let app: Application;
-		// Acquire automation API 12
+		// Acquire automation API 123
 		before(async function () {
 			app = this.app as Application;
 			terminal = app.workbench.terminal;
