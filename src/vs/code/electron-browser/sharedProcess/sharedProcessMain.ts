96935edbda13a8182eaccca390b4e806a018226a
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-04-09 09:46:58 +0200

    log

--- src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
@@ -307,7 +307,7 @@ class SharedProcessMain extends Disposable {
 		// Localizations
 		services.set(ILocalizationsService, new SyncDescriptor(LocalizationsService));
 
-		// Diagnostics123
+		// Diagnostics1234
 		services.set(IDiagnosticsService, new SyncDescriptor(DiagnosticsService));
 
 		// Settings Sync
